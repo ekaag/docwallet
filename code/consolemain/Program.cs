6f1234f3d1752e9fb96dc1d9d9e2daa9b490f342
Sundeep Ratnagiri <sandy.ratnagiri@gmail.com>
2025-02-21 21:03:59 -0800

    ci

--- code/consolemain/Program.cs
+++ code/consolemain/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+
+using docwallet.indexer;
+using System.Threading.Tasks;
+
+namespace consolemain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+            try
+            {
+                BuildStartup().Wait();
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadLine();
+            }
+            catch (System.Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        static async Task<docwallet.indexer.Startup> BuildStartup()
+        {
+            //var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            return await Task.FromResult(new docwallet.indexer.Startup());
+        }
+    }
+}
